35d9642b1f4bfe2156c5568f74e2e0a335c65900
duvoemoua <duvoemoua@gmail.com>
2025-01-29 00:02:09 -0600

    update

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -4,7 +4,33 @@ DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS subcategory;
 DROP TABLE IF EXISTS contacts;
 
---CREATE CAMPAIGN TABLE
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "email" varchar   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar   NOT NULL,
+    "category" varchar   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar   NOT NULL,
+    "subcategory" varchar   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
 CREATE TABLE "campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
@@ -18,40 +44,24 @@ CREATE TABLE "campaign" (
     "currency" VARCHAR   NOT NULL,
     "launched_date" date   NOT NULL,
     "end_date" date   NOT NULL,
-    "category" VARCHAR   NOT NULL,
-    "subcategory" VARCHAR   NOT NULL,
     "category_id" VARCHAR   NOT NULL,
     "subcategory_id" VARCHAR   NOT NULL,
-    constraint "pk_campaign" PRIMARY KEY (
-        "cf_id","contact_id","company_name","category","subcategory","category_id","subcategory_id"
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
      )
 );
 
---CREATE CATEGORY TABLE
-CREATE TABLE "category" (
-    "category_id" VARCHAR   NOT NULL,
-    "category" VARCHAR   NOT NULL,
-    constraint "pk_category" PRIMARY KEY (
-        "category_id","category"
-     )
-);
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
 
---CREATE SUBCATEGORY TABLE
-CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR   NOT NULL,
-    "subcategory" VARCHAR   NOT NULL,
-    constraint "pk_subcategory" PRIMARY KEY (
-        "subcategory_id","subcategory"
-     )
-);
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
 
---CREATE CONTACTS TABLE
-CREATE TABLE "contacts" (
-    "contact_id" int   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "email" VARCHAR   NOT NULL,
-    constraint "pk_contacts" PRIMARY KEY (
-        "contact_id","first_name","last_name","email"
-     )
-);
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+
+SELECT * FROM campaign;
+SELECT * FROM category;
+SELECT * FROM subcategory;
+SELECT * FROM contacts;
